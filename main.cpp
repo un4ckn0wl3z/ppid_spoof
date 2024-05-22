77b0ebe54de12d667f546eb675097837ea7d7ea1
Anuwat Khongchuai <gh0stsync.rt@gmail.com>
2024-05-22 14:30:55 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <memory>
+
+BOOL CreateProcessWithParant(DWORD parentId, PWSTR commandline)
+{
+	auto hProcess = ::OpenProcess(PROCESS_CREATE_PROCESS, FALSE, parentId);
+	if (!hProcess)
+	{
+		return FALSE;
+	}
+
+	SIZE_T size;
+	::InitializeProcThreadAttributeList(nullptr, 1, 0, &size);
+
+	auto buffer = std::make_unique<BYTE[]>(size);
+	auto attributes = reinterpret_cast<PPROC_THREAD_ATTRIBUTE_LIST>(buffer.get());
+	::InitializeProcThreadAttributeList(attributes, 1, 0, &size);
+
+	::UpdateProcThreadAttribute(attributes, 0,
+		PROC_THREAD_ATTRIBUTE_PARENT_PROCESS, &hProcess, sizeof(hProcess), nullptr, nullptr);
+
+	STARTUPINFOEX si = { sizeof(si) };
+	si.lpAttributeList = attributes;
+	PROCESS_INFORMATION pi;
+	BOOL created = ::CreateProcess(nullptr, commandline, nullptr, nullptr,
+		FALSE, EXTENDED_STARTUPINFO_PRESENT, nullptr, nullptr,
+		(STARTUPINFO*) & si, &pi
+	);
+
+	::CloseHandle(hProcess);
+	::DeleteProcThreadAttributeList(attributes);
+	return created;
+
+}
+
+int main(int argc, const char* argv[])
+{
+	WCHAR name[] = L"mspaint";
+	CreateProcessWithParant(22624, name);
+	return 0;
+}
